aed235498dc79a70bce40995f485d5d946d430ac
Balaji1472 <balajirama.2005@gmail.com>
2025-09-19 15:40:23 +0530

    Ready for operations

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-db_url = "add your mysql database url"
+db_url = os.getenv("DATABASE_URL") 
 engine = create_engine(db_url)
 session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
